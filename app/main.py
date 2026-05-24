bd32fcfae965fc61a9a47d118c7cb8a25ea22684
Adelia Sagitova <adeliya64775@gmail.com>
2026-05-24 17:54:11 +0500

    fix: cors policy

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ ALLOWED_ORIGINS = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
